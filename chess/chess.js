d1c762935bd727f9ab525c16692074d3e518b1fd
janvanlysebeth <jan.vanlysebeth@gmail.com>
2012-02-27 22:08:25 +0000

    'chess board changes: addPiece, pieceAt + basic display position'

--- chess/chess.js
+++ chess/chess.js
@@ -1,8 +1,11 @@
-chess = {};
+var chess = {};
 
 chess.Game = function() {
     this.position = new chess.Position();
-    this.position.addWhitePawn('a2');
+    var self = this;
+    chess.Square.VALID_FILES.forEach(function(file) {
+        self.position.addWhitePawn(file + '2');
+    });
 };
 
 chess.Position = function() {
@@ -19,14 +22,33 @@ chess.Position = function() {
         this.pieces.push(aPiece);
     };
     
-    this.addWhitePawn = function(aSquare) {
-        var p = new chess.Pawn();
-        p.isWhite = true;
-        p.rank = aSquare.charAt(1);
-        p.file = aSquare.charAt(0);
-        this.addPiece(p);
+    this.placePieceOnSquare = function(aPiece, aString) {
+        aPiece.square = new chess.Square(aString.charAt(0), aString.charAt(1));
     };
-        
+    
+    this.addPawnOn = function(aString) {
+        var p = new chess.Pawn();
+        this.addPiece(p);
+        this.placePieceOnSquare(p, aString);
+        return p;
+    };
+    
+    this.addWhitePawn = function(aString) {
+        var p = this.addPawnOn(aString);
+        p.isWhite = true;
+    };
+    
+    this.pieceAt = function(aString) {
+        var file = aString.charAt(0);
+        var rank = aString.charAt(1);
+        var piece = null;
+        this.pieces.forEach(function(aPiece){
+            if (aPiece.square.rank == rank && aPiece.square.file == file) {
+                piece = aPiece;
+            }
+        });
+        return piece;
+    };        
 };
 
 chess.Position.WHITE_KINGSIDE_CASTLING = 1;
@@ -34,11 +56,37 @@ chess.Position.WHITE_QUEENSIDE_CASTLING = 2;
 chess.Position.BLACK_KINGSIDE_CASTLING = 4;
 chess.Position.BLACK_QUEENSIDE_CASTLING = 8;
 
-chess.Piece = function() {
-    this.isWhite = true;
-    this.rank = null;
-    this.file = null;
+
+chess.Square = function(aChar, anInt) {
+    function IllegalSquareException(aChar, anInt) {
+        this.message = aChar + "" + anInt + " is not a legal square";
+    }
+    
+    $.extend(IllegalSquareException, Error);
+    
+    this.isValidRank = function(x) {
+        return (chess.Square.VALID_RANKS.indexOf(x)) != -1;
+    };
+    
+    this.isValidFile = function(x) {
+        return (chess.Square.VALID_FILES.indexOf(x)) != -1;
+    };
+    if (!this.isValidRank(anInt) || ! this.isValidFile(aChar)) {
+        throw new IllegalSquareException(aChar, anInt);
+    }
+    this.rank = anInt;
+    this.file = aChar;
 };
 
-chess.Pawn = function() {};
+chess.Square.VALID_RANKS = ['1', '2', '3', '4', '5', '6', '7', '8'];
+chess.Square.VALID_FILES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+chess.Piece = function() {
+    this.isWhite = true;
+    this.square = null;
+};
+
+chess.Pawn = function() {
+    this.image = 'Pawn';
+};
 $.extend(chess.Pawn, chess.Piece);
--- chess/chessApplication.js
+++ chess/chessApplication.js
@@ -2,17 +2,37 @@ chess.Application = function() {
     this.game = null;
     
     this.init = function() {
-        this.newGame();
+        var self = this;
+        $('#newGameButton').click(function() {
+            self.newGame();
+        });
     };
     
     this.newGame = function() {
-        var self = this;
-        $('#newGameButton').click(function () {   
-            function askUserToAbandonGame() {
-                return true;
-            }
-            if (!askUserToAbandonGame()) return null;
-            self.game = new chess.Game();
+        function askUserToAbandonGame() {
+            return true;
+        }
+        
+        if (!askUserToAbandonGame()) {
+            return null;
+        }
+        this.game = new chess.Game();
+        this.displayPosition(this.game.position);
+        return this.game;
+    };
+    
+    this.displayPosition = function (aPosition) {
+        chess.Square.VALID_RANKS.forEach(function(rank) {
+            chess.Square.VALID_FILES.forEach(function(file) {
+                var squareString = file + rank;
+                var piece = aPosition.pieceAt(squareString);
+                if (piece) {
+                    $("#" + squareString).html(
+                        "<img class='piece' src='images/BlackQueen.png'/>");
+                } else {
+                    $("#" + squareString).html("");
+                }
+            });
         });
     };
 };
